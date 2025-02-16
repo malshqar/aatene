87c256cd532390a4064ee2de771972041232cfbb
malshqar <m12342al@gmail.com>
2025-02-16 14:26:25 +0200

    create admin module with model migrations

--- Modules/Admin/Database/Migrations/2023_05_11_154503_create_admins_table.php
+++ Modules/Admin/Database/Migrations/2023_05_11_154503_create_admins_table.php
@@ -11,17 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone_number')->unique();
+            $table->json('role_name')->nullable();
             $table->string('password');
             $table->enum('status',['active','inactive'])->default('active');
-            $table->string('ban_reason')->nullable();
             $table->rememberToken();
             $table->timestamp('email_verified_at')->nullable();
-            $table->timestamp('ban_at')->nullable();
             $table->timestamp('last_active_at')->nullable();
             $table->timestamps();
         });
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 };
---
+++ Modules/Admin/Entities/Admin.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Modules\Admin\Entities;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Modules\Photo\Traits\HasPhoto;
+use Spatie\Permission\Traits\HasRoles;
+
+
+class Admin extends User
+{
+    use HasFactory, HasApiTokens, HasPhoto, HasRoles, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone_number',
+        'role_name',
+        'status',
+        'last_active_at',
+    ];
+
+
+    protected static function newFactory()
+    {
+        return \Modules\Admin\Database\factories\AdminFactory::new();
+    }
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'role_name' => 'array',
+    ];
+
+}
