58c33dae58561a96f55459c9bd3ca1ff58217643
malshqar <m12342al@gmail.com>
2025-02-19 07:03:44 +0200

    add landing page on home

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/home', function () {
-
-//     return view('dashboard::index');
-// })->name('home');
+Route::get('/', function () {
+    return view('landing');
+})->name('home');
